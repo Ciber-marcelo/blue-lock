e5ece948ac0611388b408e3a4abb5460721104f1
Ciber-marcelo <marcelones125@gmail.com>
2023-11-23 15:14:05 -0300

    feat: create-formation

---
+++ src/app/create-formation/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Blue Lock | Create formation',
+  description: 'Create formation',
+}
+
+export default function CreateFormation() {
+   return (
+      <div className="flex justify-center items-center h-[700px]">
+         Em construção
+      </div>
+   )
+}
