65cd109af84b255378ef9c21f5ad3966ba33f28c
Ciber-marcelo <marcelones125@gmail.com>
2023-12-02 02:28:54 -0300

    feat: created draggable-item

---
+++ src/components/formation-menu/draggable-item/index.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import { useDraggable } from '@dnd-kit/core';
+import { UniqueIdentifier } from '@dnd-kit/core';
+import { CSS } from '@dnd-kit/utilities';
+
+type draggItemProps = {
+   id: UniqueIdentifier
+   image: any
+   name: string
+}
+
+export function DraggableItem({id, image, name}: draggItemProps) {
+   const { attributes, listeners, setNodeRef, transform } = useDraggable({
+      id: id
+   });
+
+   const style = {
+      transform: CSS.Translate.toString(transform),
+   };
+
+   return (
+      <button
+         ref={setNodeRef}
+         style={style}
+         {...listeners}
+         {...attributes}
+         className={`
+            min-w-[140px] 
+            max-w-[140px] 
+            min-h-[170px] 
+            max-h-[170px] 
+            bg-color1
+            border-[5px]
+            border-color2 
+            rounded-md
+         `}
+      >
+         <Image
+            width={160}
+            height={160}
+            src={image}
+            alt='Character image'
+         />
+         <div className="h-[30px] bg-color2 flex justify-center pt-[10px] font-roboto text-xs text-white font-bold uppercase">
+            {name}
+         </div>
+      </button>
+   );
+}
