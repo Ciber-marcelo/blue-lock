87c423cb1918d8f94291d615b1f4af98a7488b58
Ciber-marcelo <marcelones125@gmail.com>
2023-12-04 02:00:16 -0300

    chore: edited droppable-item

--- src/components/formation-menu/droppable-item/index.tsx
+++ src/components/formation-menu/droppable-item/index.tsx
@@ -6,11 +6,19 @@ export function DroppableItem({id, children}: any) {
   });
 
   const style = {
-    opacity: isOver ? 1 : 0.5,
+    opacity: isOver ? 0.5 : 1,
   };
 
   return (
-    <div ref={setNodeRef} style={style} className='border-[5px] border-color2 w-[200px] h-[200px]'>
+    <div 
+      ref={setNodeRef} 
+      style={style} 
+      className='
+        w-[140px] 
+        h-[170px]
+        rounded-md
+        bg-gray-400
+      '>
       {children}
     </div>
   );
