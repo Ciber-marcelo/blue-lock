aa5df6d01544e0b0a1d098719cd7a6af4ca2025d
Ciber-marcelo <marcelones125@gmail.com>
2023-12-10 01:25:03 -0300

    chore: edited formations json

---
+++ public/jsons/formations.tsx
@@ -0,0 +1,36 @@
+export const formations = [
+   {
+      id: 1,
+      name: '4-3-3',
+      position: [
+         'bottom-[20px] left-[400px]', 
+         'bottom-[120px] left-[250px]',
+         'bottom-[120px] left-[550px]',
+         'bottom-[140px] left-[40px]',
+         'bottom-[140px] left-[760px]',
+         'bottom-[280px] left-[400px]',
+         'bottom-[380px] left-[250px]',
+         'bottom-[380px] left-[550px]',
+         'bottom-[520px] left-[40px]',
+         'bottom-[520px] left-[760px]',
+         'bottom-[560px] left-[400px]',
+      ]
+   },
+   {
+      id: 2,
+      name: '4-4-2',
+      position: [
+         'bottom-[20px] left-[400px]',
+         'bottom-[120px] left-[250px]',
+         'bottom-[120px] left-[550px]',
+         'bottom-[140px] left-[40px]',
+         'bottom-[140px] left-[760px]',
+         'bottom-[280px] left-[250px]',
+         'bottom-[280px] left-[550px]',
+         'bottom-[380px] left-[40px]',
+         'bottom-[380px] left-[760px]',
+         'bottom-[560px] left-[250px]',
+         'bottom-[560px] left-[550px]',
+      ]
+   },
+];
