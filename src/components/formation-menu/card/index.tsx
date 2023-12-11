b2a84b2e8fbe965f498a08a83d482eb2c40592e1
Ciber-marcelo <marcelones125@gmail.com>
2023-12-11 02:09:22 -0300

    feat: card component

---
+++ src/components/formation-menu/card/index.tsx
@@ -0,0 +1,26 @@
+export function Card() {
+   return (
+      <div
+         className='
+            flex
+            flex-col
+            items-center 
+            min-[1100px]:hidden 
+            bg-color1 
+            rounded
+            py-4
+            px-8
+            mx-4
+            my-80
+            text-white
+            font-roboto
+            font-bold 
+            uppercase
+            gap-8
+         '
+      >
+         <p>The formation page is not supported on mobile devices.</p>
+         <p>Back to Characters page</p>
+      </div>
+   );
+}
