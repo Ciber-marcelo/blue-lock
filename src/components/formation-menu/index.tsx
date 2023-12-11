66c5f6e31b1250eabfeff071818f458fc319a68a
Ciber-marcelo <marcelones125@gmail.com>
2023-12-11 03:09:55 -0300

    chore: edited formation-menu

--- src/components/formation-menu/index.tsx
+++ src/components/formation-menu/index.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react';
 import { characterList } from "../../../public/jsons/characterList"
-import { formations } from "../../../public/jsons/formations"
 import { DndContext, DragOverlay } from "@dnd-kit/core"
 import { DroppableItem } from './droppable-item';
 import { DraggableItem } from "./draggable-item";
@@ -16,19 +15,7 @@ export default function FormationMenu() {
    const [dragg, setDragg] = useState<any>([])
    const [activeDrag, setActiveDrag] = useState(null);
    const [search, setSearch] = useState('')
-   const [tactic, setTactic] = useState<any>([
-      'bottom-[10px] left-[400px]',
-      'bottom-[110px] left-[250px]',
-      'bottom-[110px] left-[550px]',
-      'bottom-[130px] left-[40px]',
-      'bottom-[130px] left-[760px]',
-      'bottom-[260px] left-[400px]',
-      'bottom-[370px] left-[250px]',
-      'bottom-[370px] left-[550px]',
-      'bottom-[500px] left-[40px]',
-      'bottom-[500px] left-[760px]',
-      'bottom-[530px] left-[400px]',
-   ])
+   const [tactic, setTactic] = useState<any>(null)
 
    useEffect(() => {
       const draggables = (
@@ -37,15 +24,59 @@ export default function FormationMenu() {
                id: item.id,
                image: item.icon,
                name: item.name,
+               rating: item.stats.total,
                selected: 'off'
             }
          ))
       );
       setDragg(draggables)
+      handleTactic('4-3-3')
    }, [])
 
-   function handleTactic(n: number) {
-      setTactic(formations[n].position)
+   function handleTactic(f: string) {
+      if (f === '4-3-3') {
+         setTactic([
+            'bottom-[10px] left-[400px]',
+            'bottom-[110px] left-[250px]',
+            'bottom-[110px] left-[550px]',
+            'bottom-[130px] left-[40px]',
+            'bottom-[130px] left-[760px]',
+            'bottom-[260px] left-[400px]',
+            'bottom-[370px] left-[250px]',
+            'bottom-[370px] left-[550px]',
+            'bottom-[500px] left-[40px]',
+            'bottom-[500px] left-[760px]',
+            'bottom-[530px] left-[400px]',
+         ])
+      } else if (f === '4-4-2') {
+         setTactic([
+            'bottom-[10px] left-[400px]',
+            'bottom-[110px] left-[250px]',
+            'bottom-[110px] left-[550px]',
+            'bottom-[130px] left-[40px]',
+            'bottom-[130px] left-[760px]',
+            'bottom-[280px] left-[280px]',
+            'bottom-[280px] left-[520px]',
+            'bottom-[380px] left-[60px]',
+            'bottom-[380px] left-[740px]',
+            'bottom-[510px] left-[250px]',
+            'bottom-[510px] left-[550px]',
+         ])
+      } else {
+         setTactic([
+            'bottom-[10px] left-[400px]',
+            'bottom-[150px] left-[400px]',
+            'bottom-[150px] left-[250px]',
+            'bottom-[150px] left-[550px]',
+            'bottom-[320px] left-[400px]',
+            'bottom-[340px] left-[230px]',
+            'bottom-[340px] left-[570px]',
+            'bottom-[360px] left-[60px]',
+            'bottom-[360px] left-[740px]',
+            'bottom-[510px] left-[250px]',
+            'bottom-[510px] left-[550px]',
+         ])
+      }
    }
 
    function handleDragStart(event: any) {
@@ -93,7 +124,6 @@ export default function FormationMenu() {
             copyDragg.splice(item.id - 1, 1, { ...item, selected: 'off' })
          ));
       }
-
       setDropp(copyDropp)
       setDragg(copyDragg)
    }
@@ -110,6 +140,7 @@ export default function FormationMenu() {
                         id={activeDrag}
                         image={dragg[activeDrag - 1].image}
                         name={dragg[activeDrag - 1].name}
+                        rating={dragg[activeDrag - 1].rating}
                      />
                   }
                </DragOverlay>
@@ -122,24 +153,25 @@ export default function FormationMenu() {
                         item.selected === 'off' && item.name.toUpperCase().includes(search.toUpperCase()) ?
                            item.id === activeDrag
                               ?
-                              <div key={activeDrag} className='min-w-[100px] min-h-[120px]'></div>
+                              <div key={activeDrag} className='min-w-[100px] min-h-[120px] border-[5px] rounded-md border-color2'></div>
                               :
                               <DraggableItem
                                  key={item.id}
                                  id={item.id}
                                  image={item.image}
                                  name={item.name}
+                                 rating={item.rating}
                               />
                            :
-                           null
+                           <div key={item.id} className='min-w-[100px] min-h-[120px] border-[5px] rounded-md border-color2'></div>
                      ))}
                   </div>
 
                   <div className='flex flex-col gap-2'>
                      <div className='flex gap-2'>
-                        <ButtonDnd onClick={() => handleTactic(0)}>4-3-3</ButtonDnd>
-                        <ButtonDnd onClick={() => handleTactic(1)}>4-4-2</ButtonDnd>
-                        <ButtonDnd onClick={() => handleTactic(2)}>3-5-2</ButtonDnd>
+                        <ButtonDnd onClick={() => handleTactic('4-3-3')}>4-3-3</ButtonDnd>
+                        <ButtonDnd onClick={() => handleTactic('4-4-2')}>4-4-2</ButtonDnd>
+                        <ButtonDnd onClick={() => handleTactic('3-5-2')}>3-5-2</ButtonDnd>
                      </div>
 
                      <div className='relative flex justify-center items-center w-[900px] h-[660px] bg-fieldBg rounded-md'>
@@ -153,6 +185,7 @@ export default function FormationMenu() {
                                        id={dragg[item - 1].id}
                                        image={dragg[item - 1].image}
                                        name={dragg[item - 1].name}
+                                       rating={dragg[item - 1].rating}
                                     />
                                  }
                               </DroppableItem>
