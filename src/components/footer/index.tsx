f396b86961efc9b251d2f5c9a8ba6605e0c473b5
Ciber-marcelo <marcelones125@gmail.com>
2024-01-07 00:43:03 -0300

    chore: added github link on footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className="flex justify-center items-center gap-2 mt-16 py-8 bg-color1 font-roboto text-white font-bold">
-      <Image 
-        className="w-12 h-12 bg-white p-1 rounded-md"
-        width={30}
-        height={30}
-        src="/images/M.svg"
-        alt="Logo marcelones"
-        draggable={false}
-      />
+      <Link href={'https://github.com/Ciber-marcelo/blue-lock'} target="blank">
+        <Image
+          className="w-12 h-12 bg-white p-1 rounded-md"
+          width={30}
+          height={30}
+          src="/images/M.svg"
+          alt="Logo marcelones"
+          draggable={false}
+        />
+      </Link>
       Developed by Marcelones
+
     </div>
   )
 }
