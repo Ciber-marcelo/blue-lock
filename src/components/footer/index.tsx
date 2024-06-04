b0c57709528efefc6767ac81268d3b33c663929b
Ciber-marcelo <marcelones125@gmail.com>
2024-06-03 21:38:39 -0300

    chore: edit footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="flex justify-center items-center gap-2 mt-16 py-8 bg-color1 font-roboto text-white font-bold">
-      <Link href={'https://github.com/Ciber-marcelo/blue-lock'} target="blank">
+    <div className="flex justify-center bg-color1 py-8 px-6 mt-16">
+      <a href="https://github.com/Ciber-marcelo/blue-lock" target='blank' className="flex">
         <Image
-          className="w-12 h-12 bg-white p-1 rounded-md"
+          className='w-12 h-12 bg-color1 border-4 border-color2 rounded-s-md p-1'
           width={30}
           height={30}
-          src="/images/M.svg"
-          alt="Logo marcelones"
-          draggable={false}
+          src='/images/M.svg'
+          alt='Logo Marcelo'
         />
-      </Link>
-      Developed by Marcelones
-
+        <p className="h-12 flex items-center px-4 rounded-e-md border-4 border-l-0 bg-white border-color2 font-roboto font-bold text-color2">
+          Desenvolvido por Marcelones
+        </p>
+      </a>
     </div>
   )
 }
