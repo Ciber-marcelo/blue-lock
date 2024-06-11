4e30847d24b046d002eb2c20bef3ae99a8f5298d
Ciber-marcelo <marcelones125@gmail.com>
2024-06-10 23:16:17 -0300

    chore: edit char-list

--- src/components/char-list/index.tsx
+++ src/components/char-list/index.tsx
@@ -5,26 +5,39 @@ import { useContext, useState } from "react";
 import Search from "../search";
 
 export default function CharList() {
-   const [search, setSearch] = useState('')
-   const { selectedChar } = useContext(CharContext);
+   const {selectedChar} = useContext(CharContext);
+   const [charsFilter, setCharsFilter] = useState<object[]>(characterList)
+
+   function filter(search: string) {
+      const fil: object[] = []
+
+      characterList.map((item) => (
+         item.name.toUpperCase().includes(search.toUpperCase()) && fil.push(item)
+      ))
+
+      setCharsFilter(fil)
+   }
 
    return (
       <div className="container pt-16">
-         <Search onChange={(e: any) => setSearch(e.target.value.toUpperCase())}/>
+         <Search onChange={(e: any) => filter(e.target.value.toUpperCase())} />
 
          <div className="overflow-x-scroll scrollbar pt-8 pb-2">
             <div className="flex gap-4 h-[170px]">
-               {characterList.map((item) => (
-                  item.name.toUpperCase().includes(search.toUpperCase()) ?
-                  <CharItem
-                     onClick={() => selectedChar(item)}
-                     key={item.id}
-                     image={item.icon}
-                     name={item.name}
-                  />
+               {charsFilter.length !== 0 ?
+                  charsFilter.map((item: any) => (
+                     <CharItem
+                        onClick={() => selectedChar(item)}
+                        key={item.id}
+                        image={item.icon}
+                        name={item.name}
+                     />
+                  ))
                   :
-                  null
-               ))}
+                  <div className="flex w-full justify-center items-center font-roboto text-2xl text-color2 font-bold">
+                     NO FOUND
+                  </div>
+               }
             </div>
          </div>
       </div>
